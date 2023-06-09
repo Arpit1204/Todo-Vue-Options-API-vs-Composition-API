be245846b7fa33fec035e1897ec8a4606d718292
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-06-09 16:44:10 +0530

    comments removed

--- src/views/Composition.vue
+++ src/views/Composition.vue
@@ -1,168 +1,108 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h3>You have {{todosCount}} Todos!</h3>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h3>You have {{ todosCount }} Todos!</h3>
     <div>
       <input
-      @keyup.enter="addTodo"
+        @keyup.enter="addTodo"
         v-model="newTodo"
-        type="text" 
-        placeholder="Add a Todos">
+        type="text"
+        placeholder="Add a Todos"
+      />
     </div>
     <div>
       <ul v-for="(todo, index) in todos" :key="todo.id">
         <li>
-          <span>{{todo.name}}</span>
+          <span>{{ todo.name }}</span>
           <button @click="deleteTodo(index)">X</button>
         </li>
-        </ul>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-import { computed, ref, watch } from 'vue'
+import { computed, ref, watch } from "vue";
 
 export default {
-  setup(){
-    let newTodo = ref('');
-    let matchedWord = ref(null)
+  setup() {
+    let newTodo = ref("");
+    let matchedWord = ref(null);
     let todos = ref([
       {
-          id:1,
-          name:'One'
-        },
-        {
-          id:1,
-          name:'Two'
-        },
-        {
-          id:1,
-          name:'Three'
-        }
-    ])
+        id: 1,
+        name: "One",
+      },
+      {
+        id: 1,
+        name: "Two",
+      },
+      {
+        id: 1,
+        name: "Three",
+      },
+    ]);
 
-    const swearWords = ['Idiot', 'Murder', 'Sucide' , 'Kill' ]
+    const swearWords = ["Idiot", "Murder", "Sucide", "Kill"];
 
-    const addTodo = ()=>{
+    const addTodo = () => {
       let newTodoObj = {
-        id:Date.now(),
-        name:newTodo.value
+        id: Date.now(),
+        name: newTodo.value,
+      };
+      if (newTodo.value) {
+        todos.value.push(newTodoObj);
       }
-      if(newTodo.value){
+      newTodo.value = "";
+    };
 
-        todos.value.push(newTodoObj)
-      }
-      newTodo.value = '';
-    }
+    const deleteTodo = (elt) => {
+      todos.value.splice(elt, 1);
+    };
 
+    const todosCount = computed(() => {
+      return todos.value.length;
+    });
 
-    const deleteTodo=(elt)=>{
-      todos.value.splice(elt, 1)
-    }
-
-    const todosCount= computed(()=>{
-      return todos.value.length
-    })
-
-    watch(newTodo, (e)=>{
+    watch(newTodo, (e) => {
       console.log(e);
-      for(let i=0; i<swearWords.length; i++){
-        if(newTodo.value.toLowerCase().includes(swearWords[i].toLowerCase())){
-          matchedWord.value = swearWords[i]
-          newTodo.value = ''
-        alert('Bad Word Detected 🚨 ' + `"${matchedWord.value}"` + ' !!!!')
+      for (let i = 0; i < swearWords.length; i++) {
+        if (newTodo.value.toLowerCase().includes(swearWords[i].toLowerCase())) {
+          matchedWord.value = swearWords[i];
+          newTodo.value = "";
+          alert("Bad Word Detected 🚨 " + `"${matchedWord.value}"` + " !!!!");
         }
       }
-    })
-    return{
+    });
+    return {
       newTodo,
       todos,
       swearWords,
       addTodo,
       deleteTodo,
-      todosCount
-    }
-  }
-  // data(){
-  //   return{
-  //     newTodo:'',
-  //     matchedWord:null,
-  //     todos:[
-  //       {
-  //         id:1,
-  //         name:'One'
-  //       },
-  //       {
-  //         id:1,
-  //         name:'Two'
-  //       },
-  //       {
-  //         id:1,
-  //         name:'Three'
-  //       }
-  //     ], 
-      
-  //     swearWords:['Idiot', 'Murder', 'Sucide' , 'Kill' ]
-  //   }
-  // },
-  // methods:{
-  //   addTodo(){
-  //     let newTodoObj = {
-  //       id:Date.now(),
-  //       name:this.newTodo
-  //     }
-  //     if(this.newTodo){
-
-  //       this.todos.push(newTodoObj)
-  //     }
-  //     this.newTodo = '';
-  //   }, 
-  //   deleteTodo(elt){
-  //     this.todos.splice(elt, 1)
-  //   }
-  // },
-  //  computed:{
-  //   todosCount(){
-  //     return this.todos.length
-  //   }
-  // }, 
-  // watch :{
-  //   newTodo(newvalue){
-  //     console.log(newvalue);
-  //     for(let i=0; i<this.swearWords.length; i++){
-  //       if(this.newTodo.toLowerCase().includes(this.swearWords[i].toLowerCase())){
-  //         this.matchedWord = this.swearWords[i]
-  //         this.newTodo = ''
-  //       alert('Bad Word Detected 🚨 ' + `"${this.matchedWord}"` + ' !!!!')
-  //       }
-  //     }
-      
-        
-        
-  //     // }
-  //   }
-  // }
-  
-}
+      todosCount,
+    };
+  },
+};
 </script>
 
 
-  <style>
-  ul{
-    list-style: none;
-    padding: 0;
-    width: 200px;
-    margin: 20px auto 0;
-  }
-  li{
-    border: 1px solid;
-    display: flex;
-    margin-bottom: 10px;
-    align-items: center;
-  }
-   li span{
-    flex-grow: 1;
-   }
-  
-  </style>
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+  width: 200px;
+  margin: 20px auto 0;
+}
+
+li {
+  border: 1px solid;
+  display: flex;
+  margin-bottom: 10px;
+  align-items: center;
+}
+
+li span {
+  flex-grow: 1;
+}
+</style>
--- src/views/Options.vue
+++ src/views/Options.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h3>You have {{todosCount}} Todos!</h3>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h3>You have {{ todosCount }} Todos!</h3>
     <div>
       <input
-      @keyup.enter="addTodo"
+        @keyup.enter="addTodo"
         v-model="newTodo"
-        type="text" 
-        placeholder="Add a Todos">
+        type="text"
+        placeholder="Add a Todos"
+      />
     </div>
     <div>
       <ul v-for="(todo, index) in todos" :key="todo.id">
         <li>
-          <span>{{todo.name}}</span>
+          <span>{{ todo.name }}</span>
           <button @click="deleteTodo(index)">X</button>
         </li>
-        </ul>
+      </ul>
     </div>
   </div>
 </template>
@@ -24,85 +25,82 @@
 // @ is an alias to /src
 
 export default {
-  data(){
-    return{
-      newTodo:'',
-      matchedWord:null,
-      todos:[
+  data() {
+    return {
+      newTodo: "",
+      matchedWord: null,
+      todos: [
         {
-          id:1,
-          name:'One'
+          id: 1,
+          name: "One",
         },
         {
-          id:1,
-          name:'Two'
+          id: 1,
+          name: "Two",
         },
         {
-          id:1,
-          name:'Three'
-        }
-      ], 
-      
-      swearWords:['Idiot', 'Murder', 'Sucide' , 'Kill' ]
-    }
-  },
-  methods:{
-    addTodo(){
-      let newTodoObj = {
-        id:Date.now(),
-        name:this.newTodo
-      }
-      if(this.newTodo){
+          id: 1,
+          name: "Three",
+        },
+      ],
 
-        this.todos.push(newTodoObj)
-      }
-      this.newTodo = '';
-    }, 
-    deleteTodo(elt){
-      this.todos.splice(elt, 1)
-    }
+      swearWords: ["Idiot", "Murder", "Sucide", "Kill"],
+    };
   },
-   computed:{
-    todosCount(){
-      return this.todos.length
-    }
-  }, 
-  watch :{
-    newTodo(newvalue){
+  methods: {
+    addTodo() {
+      let newTodoObj = {
+        id: Date.now(),
+        name: this.newTodo,
+      };
+      if (this.newTodo) {
+        this.todos.push(newTodoObj);
+      }
+      this.newTodo = "";
+    },
+    deleteTodo(elt) {
+      this.todos.splice(elt, 1);
+    },
+  },
+  computed: {
+    todosCount() {
+      return this.todos.length;
+    },
+  },
+  watch: {
+    newTodo(newvalue) {
       console.log(newvalue);
-      for(let i=0; i<this.swearWords.length; i++){
-        if(this.newTodo.toLowerCase().includes(this.swearWords[i].toLowerCase())){
-          this.matchedWord = this.swearWords[i]
-          this.newTodo = ''
-        alert('Bad Word Detected 🚨 ' + `"${this.matchedWord}"` + ' !!!!')
+      for (let i = 0; i < this.swearWords.length; i++) {
+        if (
+          this.newTodo.toLowerCase().includes(this.swearWords[i].toLowerCase())
+        ) {
+          this.matchedWord = this.swearWords[i];
+          this.newTodo = "";
+          alert("Bad Word Detected 🚨 " + `"${this.matchedWord}"` + " !!!!");
         }
       }
-      
-        
-        
+
       // }
-    }
-  }
-  
-}
+    },
+  },
+};
 </script>
 
 
   <style>
-  ul{
-    list-style: none;
-    padding: 0;
-    width: 200px;
-    margin: 20px auto 0;
-  }
-  li{
-    border: 1px solid;
-    display: flex;
-    margin-bottom: 10px;
-    align-items: center;
-  }
-   li span{
-    flex-grow: 1;
-   }
-  
-  </style>
+ul {
+  list-style: none;
+  padding: 0;
+  width: 200px;
+  margin: 20px auto 0;
+}
+li {
+  border: 1px solid;
+  display: flex;
+  margin-bottom: 10px;
+  align-items: center;
+}
+li span {
+  flex-grow: 1;
+}
+</style>
